396d32044b2e1da6d1797f2bd64b1cc9273190c4
Michael Amuah <mkdamuah@hotmail.com>
2022-12-18 18:58:39 +0000

    header update

--- monty.h
+++ monty.h
@@ -36,4 +36,8 @@ typedef struct instruction_s
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
+
+int push(stack_t* stackPtr, int value);
+void push(stack_t **h, unsigned int line_number, const char *n);
+
 #endif
